64c6a2a3f673e9afcbaacfa607ee8cd44708aa75
Venkatesh Prasad V M <venkateshprasadvm@gmail.com>
2022-04-03 20:56:30 +0530

    fix: Removes github stars

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
@@ -68,25 +68,25 @@ const StyledCredit = styled.div`
 `;
 
 const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
+  // const [githubInfo, setGitHubInfo] = useState({
+  //   stars: null,
+  //   forks: null,
+  // });
 
   useEffect(() => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
+    // fetch('https://api.github.com/repos/bchiang7/v4')
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     const { stargazers_count, forks_count } = json;
+    //     setGitHubInfo({
+    //       stars: stargazers_count,
+    //       forks: forks_count,
+    //     });
+    //   })
+    //   .catch(e => console.error(e));
   }, []);
 
   return (
@@ -108,7 +108,7 @@ const Footer = () => {
         <a href="https://github.com/bchiang7/v4">
           <div>Redesigned by vedartm forked from BC</div>
 
-          {githubInfo.stars && githubInfo.forks && (
+          {/* {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
               <span>
                 <Icon name="Star" />
@@ -119,7 +119,7 @@ const Footer = () => {
                 <span>{githubInfo.forks.toLocaleString()}</span>
               </span>
             </div>
-          )}
+          )} */}
         </a>
       </StyledCredit>
     </StyledFooter>
